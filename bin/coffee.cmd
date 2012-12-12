9f87d65fcc2d1c0c9a17725aff74d24302cac7c5
Klaus Rennecke <alagopus@gmail.com>
2012-12-12 18:28:55 +0100

    starter scripts for some OS

---
+++ bin/coffee.cmd
@@ -0,0 +1,7 @@
+@echo off
+setlocal
+set TOP=%~dp0\..
+IF NOT EXIST "%TOP%\lib\coffee-script" set TOP=%TOP%\packages\coffee-script
+cd %TOP%\lib\coffee-script
+node -e "require('./command').run()" %*
+

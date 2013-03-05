e21900ea4e813875d6656203b612cd466557e506
Klaus Rennecke <a.lagopus@gmail.com>
2013-03-05 16:06:59 +0100

    fix syntax error in rhino launcher

--- bin/rhino.cmd
+++ bin/rhino.cmd
@@ -2,7 +2,7 @@
 setlocal
 set TOP=%~dp0\..
 IF NOT EXIST "%TOP%\lib\coffee-script" set TOP=%TOP%\packages\coffee-script
-set CP=$top\jar\js.jar;$top\jar\jna-3.5.1.jar;$top\jar\platform-3.5.1.jar
+set CP=%TOP%\jar\js.jar;%TOP%\jar\jna-3.5.1.jar;%TOP%\jar\platform-3.5.1.jar
 rem set MAIN=org.mozilla.javascript.tools.debugger.Main
-main="-Djava.awt.headless=true org.mozilla.javascript.tools.shell.Main"
-java -cp %CP% %MAIN% -version 180 -modules $top\lib\commonjs -modules $top\lib\coffee-script "$@"
+set MAIN=-Djava.awt.headless=true org.mozilla.javascript.tools.shell.Main
+java -cp %CP% %MAIN% -version 180 -modules %TOP%\lib\commonjs -modules %TOP%\lib\coffee-script %*
